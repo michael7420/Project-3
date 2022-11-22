a16cbdbd4b1ae3fd1fc625533cdc6ecc851949b0
Michael Szewczyk <mszewczyk@hawk.iit.edu>
2022-11-21 21:20:50 -0600

    Commit 5
    Added scripts, filled out a majority of map.js

--- js/map.js
+++ js/map.js
@@ -1,12 +1,29 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: ,
-    center: { lat: , lng: },
+    zoom: 3,
+    center: { lat: 41.880383, lng: -87.739711},
   });
   
-  var marker = new google.maps.Marker({
-    position: { lat: , lng: },
-    map: map,
+  var childhood = new google.maps.Circle({
+    center:{ lat:41.982300, lng: -87.807325 },
+    radius:1500,
+    strokeColor:"#FF0000",
+    strokeOpacity:0.8,
+    strokeWeight:2,
+    fillColor:"#FF0000",
+    fillOpacity:0.2,
+    map: map
+  });
+  
+  var school = new google.maps.Circle({
+    center:{ lat:41.834793, lng: -87.626406},
+    radius:600,
+    strokeColor:"#FF0000",
+    strokeOpacity:0.8,
+    strokeWeight:2,
+    fillColor:"#FF0000",
+    fillOpacity:0.2,
+    map: map
   });
   
   var infowindow = new google.maps.InfoWindow({
