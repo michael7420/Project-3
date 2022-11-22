54307bbb3e8ff1c120e4f326fe951fe7f0e5c6e7
Michael Szewczyk <mszewczyk@hawk.iit.edu>
2022-11-21 22:52:46 -0600

    Commit 9
    Changes made to resolve map issue

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 11,
     center: { lat: 41.880383, lng: -87.739711},
   });
   
@@ -15,20 +15,14 @@ function initMap() {
     map: map
   });
   
-  var school = new google.maps.Circle({
-    center:{ lat:41.834793, lng: -87.626406},
-    radius:600,
-    strokeColor:"#FF0000",
-    strokeOpacity:0.8,
-    strokeWeight:2,
-    fillColor:"#FF0000",
-    fillOpacity:0.2,
-    map: map
+  var marker = new google.maps.Marker({
+    position: { lat:41.834214, lng:-87.627399 },
+    map: map,
   });
   
   var infowindow = new google.maps.InfoWindow({
     content:""
   });
-  infowindow.open(map, var);
+  infowindow.open(map, marker);
 }
 window.initMap = initMap;
